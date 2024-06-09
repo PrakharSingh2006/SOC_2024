032264d1b1282467c0580e95b610457d021eeed7
Prakhar Singh <140534377+PrakharSingh2006@users.noreply.github.com>
2024-06-10 01:16:52 +0530

    Create spiral matrix.cpp

---
+++ CHECKPOINT_1/Math & Geometry/spiral matrix.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    vector<int> spiralOrder(vector<vector<int>>& matrix) {
+        vector<int> result;
+        if (matrix.empty() || matrix[0].empty()) return result;
+
+        int top = 0, bottom = matrix.size() - 1;
+        int left = 0, right = matrix[0].size() - 1;
+
+        while (top <= bottom && left <= right) {
+            // Traverse from left to right
+            for (int j = left; j <= right; ++j) {
+                result.push_back(matrix[top][j]);
+            }
+            ++top;
+
+            // Traverse from top to bottom
+            for (int i = top; i <= bottom; ++i) {
+                result.push_back(matrix[i][right]);
+            }
+            --right;
+
+            if (top <= bottom) {
+                // Traverse from right to left
+                for (int j = right; j >= left; --j) {
+                    result.push_back(matrix[bottom][j]);
+                }
+                --bottom;
+            }
+
+            if (left <= right) {
+                // Traverse from bottom to top
+                for (int i = bottom; i >= top; --i) {
+                    result.push_back(matrix[i][left]);
+                }
+                ++left;
+            }
+        }
+
+        return result;
+    }
+};
